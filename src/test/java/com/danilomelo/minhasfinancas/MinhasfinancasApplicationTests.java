b716732a9a9b805ba9aa49f05a96de6938b6b5e2
dsm1994 <danilosm94@hotmail.com>
2022-08-03 04:41:29 -0300

    Modificacoes no backend

--- src/test/java/com/danilomelo/minhasfinancas/MinhasfinancasApplicationTests.java
+++ src/test/java/com/danilomelo/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -1,13 +1,8 @@
 package com.danilomelo.minhasfinancas;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MinhasfinancasApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
