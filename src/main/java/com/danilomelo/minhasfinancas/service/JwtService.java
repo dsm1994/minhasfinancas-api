f1aabb9b10123d8fbd3d82e8434af7c1f0a99e51
dsm1994 <danilosm94@hotmail.com>
2022-08-09 15:04:42 -0300

    Atualizacao do backend

---
+++ src/main/java/com/danilomelo/minhasfinancas/service/JwtService.java
@@ -0,0 +1,14 @@
+package com.danilomelo.minhasfinancas.service;
+
+import com.danilomelo.minhasfinancas.model.entity.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+
+public interface JwtService {
+	
+	String gerarToken(Usuario usuario);
+	Claims obterClaims(String token) throws ExpiredJwtException;
+	boolean isTokenValido(String token);
+	String obterLoginUsuario(String token);
+}
--- src/main/java/com/danilomelo/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/danilomelo/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -2,6 +2,7 @@ package com.danilomelo.minhasfinancas.service.impl;
 
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +16,12 @@ import com.danilomelo.minhasfinancas.service.UsuarioService;
 public class UsuarioServiceImpl implements UsuarioService {
 
 	private UsuarioRepository repository;
+	private PasswordEncoder encoder;
 	
-	public UsuarioServiceImpl(UsuarioRepository repository) {
+	public UsuarioServiceImpl(UsuarioRepository repository, PasswordEncoder encoder) {
 		super();
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 
 	@Override
@@ -28,7 +31,8 @@ public class UsuarioServiceImpl implements UsuarioService {
 			throw new ErroAutenticacao("Usuário não encontrado para o email informado.");
 		}
 		
-		if (!usuario.get().getSenha().equals(senha)) {
+		boolean senhasBatem = encoder.matches(senha, usuario.get().getSenha());
+		if (!senhasBatem) {
 			throw new ErroAutenticacao("Senha inválida."); 
 		}
 		return usuario.get();
@@ -38,7 +42,14 @@ public class UsuarioServiceImpl implements UsuarioService {
 	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
 		validarEmail(usuario.getEmail());
+		criptografarSenha(usuario);
 		return repository.save(usuario);
+	}
+
+	private void criptografarSenha(Usuario usuario) {
+		String senha = usuario.getSenha();
+		String senhaCripto = encoder.encode(senha);
+		usuario.setSenha(senhaCripto);
 	}
 
 	@Override
